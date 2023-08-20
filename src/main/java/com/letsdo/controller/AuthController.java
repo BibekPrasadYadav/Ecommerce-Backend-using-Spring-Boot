3146cebaf9fc1e74db4971d2753076cb45b800fe
Bibek <bibek.roman1@gmail.com>
2023-08-21 00:54:06 +0530

    Login and Signup Authenticated

--- src/main/java/com/letsdo/controller/AuthController.java
+++ src/main/java/com/letsdo/controller/AuthController.java
@@ -1,9 +1,92 @@
 package com.letsdo.controller;
 
+import com.letsdo.config.JwtHelper;
+import com.letsdo.exception.UserException;
+import com.letsdo.model.User;
+import com.letsdo.repository.UserRepository;
+import com.letsdo.request.LoginRequest;
+import com.letsdo.response.AuthResponse;
+import com.letsdo.service.CustomerUserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private JwtHelper jwtHelper;
+
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private CustomerUserServiceImpl customerUserService;
+    @PostMapping("/signup")
+    public ResponseEntity<AuthResponse>createUserHandler(@RequestBody User user)throws UserException{
+        String email=user.getEmail();
+        String password= user.getPassword();
+        String firstName=user.getFirstName();
+        String lastName= user.getLastName();
+
+        User isEmailExist=userRepository.findByEmail(email);
+        if(isEmailExist!=null){
+            throw new UserException("Email is already used with another account");
+        }
+
+        User createdUser=new User();
+        createdUser.setEmail(email);
+        createdUser.setPassword(passwordEncoder.encode(password));
+        createdUser.setFirstName(firstName);
+        createdUser.setLastName(lastName);
+
+        User savedUser=userRepository.save(createdUser);
+        Authentication authentication=new UsernamePasswordAuthenticationToken(savedUser.getEmail(),savedUser.getPassword());
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String token=jwtHelper.generateToken(authentication);
+
+        AuthResponse authResponse=new AuthResponse(token,"Signup Successfully");
+        return new ResponseEntity<AuthResponse>(authResponse, HttpStatus.CREATED);
+
+    }
+
+
+    @PostMapping("/signin")
+    public ResponseEntity<AuthResponse>loginUserHandler(@RequestBody LoginRequest loginRequest)throws UserException{
+        String username=loginRequest.getEmail();
+        String password=loginRequest.getPassword();
+
+        Authentication authentication=authenticate(username,password);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        String token=jwtHelper.generateToken(authentication);
+
+        AuthResponse authResponse=new AuthResponse(token,"Sign In Successfully");
+        return new ResponseEntity<AuthResponse>(authResponse, HttpStatus.CREATED);
+    }
+
+    private Authentication authenticate(String username, String password) {
+        UserDetails userDetails= customerUserService.loadUserByUsername(username);
+        if(userDetails==null){
+            throw new BadCredentialsException("Invalid Username");
+        }
+
+        if(!passwordEncoder.matches(password,userDetails.getPassword())){
+            throw new BadCredentialsException("Invalid Password");
+        }
+        return new UsernamePasswordAuthenticationToken(userDetails,null,userDetails.getAuthorities());
+    }
 }
---
+++ src/main/java/com/letsdo/service/CustomerUserServiceImpl.java
@@ -0,0 +1,35 @@
+package com.letsdo.service;
+
+import com.letsdo.model.User;
+import com.letsdo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CustomerUserServiceImpl implements UserDetailsService {
+
+
+    private UserRepository userRepository;
+
+    private CustomerUserServiceImpl(UserRepository userRepository){
+        this.userRepository=userRepository;
+    }
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user=userRepository.findByEmail(username);
+        if(user==null){
+            throw new UsernameNotFoundException(username);
+        }
+
+        List<GrantedAuthority> authorities=new ArrayList<>();
+
+        return new org.springframework.security.core.userdetails.User(user.getEmail(),user.getPassword(),authorities);
+    }
+}
